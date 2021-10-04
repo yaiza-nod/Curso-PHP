e9758b2317f3ad7685107ff0afb478753896697b
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-04 08:33:47 +0200

    Primer Commit: Hola Mundo

---
+++ 1-Hola-Mundo/holaMundo.php
@@ -0,0 +1,30 @@
+<?php
+
+// echo nos sirve para mostrar info en pantalla
+
+# Otro comentario
+
+/*
+Otro
+comentario
+*/
+
+// echo("Hola Mundo!!");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1><?php echo("Hola Mundo!! Soy un H1!! :)"); ?></h1>
+    
+</body>
+</html>
