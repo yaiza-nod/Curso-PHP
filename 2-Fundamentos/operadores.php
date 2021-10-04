7b8f077dbfb9c25038bf7d72eaa8d5612c28d184
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-04 10:53:08 +0200

    Haciendo la parte de los Operadores

---
+++ 2-Fundamentos/operadores.php
@@ -0,0 +1,111 @@
+<?php
+
+    /*
+    Operadores Artiméticos:
+
+    + Suma
+    - Resta
+    * Multiplicación
+    / División
+
+    */
+
+    // $numero1 = 10;
+    // $numero2 = 5;
+
+    /*# Suma: 
+
+    $resultado = $numero1 + $numero2;
+
+    # Resta:
+
+    $resultado = $numero1 - $numero2;
+
+    # Mutiplicación:
+
+    $resultado = $numero1 * $numero2;
+
+    # División: 
+
+    $resultado = $numero1 / $numero2;
+
+    # Modulo:
+
+    $resultado = $numero1 % $numero2;
+
+    echo $resultado;*/
+
+    /* $numero1 += 7; // sumo 7 al numero1 (da 17)
+
+    echo $numero1;*/
+
+    /*
+    Operadores de Comparación:
+
+    == Igualdad
+    === Idéntico (el igual en tipo y en valor)
+    !=, <>
+    !==
+    >
+    <
+    >=
+    <=
+
+    */
+
+    /*
+    
+    $num = 10;
+    $variable = true;
+
+    if ($variable === true) {
+        echo 'Es idéntico';
+    } else {
+        echo 'No es idéntico.';
+    };*/
+
+    /*
+    Operadores lógicos:
+
+    and, &&
+    or, ||
+    xor
+    !
+
+    $num = 10;
+
+    if ($num >= 10 && num < 20) {
+        echo 'Ejecuta';
+    };
+
+    */
+
+    /*
+    Operadores de Incremento / Decremento:
+
+    ++$x
+    $x++
+    --$x
+    $x--
+
+    */
+
+    /*$num = 10;
+
+    echo $num++; //--> si los pongo después sigue dando 10 porque primero muestra el numero y luego suma
+    
+    echo '<br />'.$num; // este ya muestra 11 porque lo ha sumado antes depués de <mostrarlo></mostrarlo>*/
+
+    /*
+    Operadores de Cadenas:
+    .
+    .=
+
+    */
+
+    /*$texto = 'Cadena de texto';
+    $texto .= ' Segunda cadena de texto'; // así añades una cadena a una cadena ya existente
+
+    echo $texto;*/
+
+?>
