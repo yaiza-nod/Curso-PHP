e28401e8ac62c0970c7e323c23a7a7f487f60b6c
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-04 10:30:02 +0200

    Haciendo la parte 2 de Condicionales: If-Else-Else If

---
+++ 2-Fundamentos/condicional-if-else.php
@@ -0,0 +1,40 @@
+<?php
+
+    // SENTENCIA ELSE
+
+    /* $edad = 18;
+
+    if ($edad >= 18) {
+
+        echo 'Bienvenido, eres mayor de edad!!';
+    } else {
+        echo 'Eres menor de edad, no puedes continuar.';
+    };*/ 
+
+    // ELSE IF
+
+    $mes = 'Junio';
+    $verdaero = false;
+
+    /*if ($mes === 'Diciembre') {
+        echo 'Feliz Navidad!!!';
+
+    } else if ($mes === 'Enero') {
+        echo 'Feliz Año Nuevo!!!';
+
+    } else if ($mes === 'Julio') {
+        echo 'Felices Vacaciones!!!';
+
+    } else {
+
+        echo 'El mes no tiene celebración.';
+    };*/
+
+    if ($verdadero) {
+        echo 'El valor es true';
+
+    } else {
+        echo 'El valor es false';
+    }
+
+?>
