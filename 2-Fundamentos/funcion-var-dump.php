344930b54fcc292b609adc15a53c6ca319506ebb
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-05 08:41:20 +0200

    Haciendo la parte de funcion var_dump()

---
+++ 2-Fundamentos/funcion-var-dump.php
@@ -0,0 +1,14 @@
+<?php
+
+    $texto = 'Yaiza';
+    $numero = 9;
+    $numero2 = '3';
+    $array = ['Yaiza', 'Alvaro', 'Iris'];
+    $array_asoc = ['Nombre' => 'Yaiza', 'Edad' => 22];
+    $booleano = false;
+
+    echo '<pre>';
+    var_dump($texto); // no necesito poner echo para que lo muestre en navegador
+    echo '</pre>';
+
+?>
