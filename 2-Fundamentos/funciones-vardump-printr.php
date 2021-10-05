9ab1f5c4b9dedf084e64300ddd5b722129959462
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-05 08:46:36 +0200

    Haciendo las funciones vardump y printr

--- 2-Fundamentos/funciones-vardump-printr.php
+++ 2-Fundamentos/funciones-vardump-printr.php
@@ -7,8 +7,16 @@
     $array_asoc = ['Nombre' => 'Yaiza', 'Edad' => 22];
     $booleano = false;
 
-    echo '<pre>';
+    /*echo '<pre>';
     var_dump($texto); // no necesito poner echo para que lo muestre en navegador
+    echo '</pre>';*/
+
+    echo '<pre>';
+    print_r($array);
     echo '</pre>';
+    // no muestra el tipo, esa es la diferencia con var_dump
+    // print_r muestra solo el valor de la variable
+
+
 
 ?>
