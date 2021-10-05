f2dc08abe4e5c0f6ca74d03b8bc78a6e6e95f5ae
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-05 11:16:49 +0200

    Haciendo la parte de las declaracion de tipo de devolucion

--- php7/declaraciones-devolucion.php
+++ php7/declaraciones-devolucion.php
@@ -1,5 +1,7 @@
 <?php
 
+    declare(strict_types=1); // estrictamente debe ser int, aunque ponga '8' lo cogerá como string
+
     // Permite especificar qué tipo de valor queremos devolver
 
     function obtenerEdad() : int {
