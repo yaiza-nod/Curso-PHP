fbf9ac66ec7d8ffb4e0a50b5c2c4cb60951474f6
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-05 09:58:06 +0200

    Haciendo la parte de funciones Matematicas

---
+++ 2-Fundamentos/funciones-matematicas.php
@@ -0,0 +1,13 @@
+<?php
+
+    $numero = 15.3699;
+
+    # echo round($numero, 1); // redondea un numero pasado como parametro, con , digo el numero de decimales
+
+    # echo rand(0, 10); // numero al azar entre los dos que pase por parametro
+
+    # echo ceil(12.001); // redondea siempre hacia arriba, es decir en este caso dará 13
+
+    echo M_PI; // no es un funcion como tal, sino que php ya tiene el numero pi como una constante establecida
+
+?>
