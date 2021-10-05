1ab2d3b640c0c2651372dc5684c45d8770052a97
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-05 08:56:10 +0200

    Haciendo la introduccion a las funciones

---
+++ 2-Fundamentos/funciones.php
@@ -0,0 +1,24 @@
+<?php
+
+    /* 
+    funciones vistas:
+
+    count();
+    sort();
+    rsort();
+    var_dump();
+    print_r();
+    */
+
+    function saludo () {
+        echo 'Hola, usuario!!!'.'<br />';
+
+    };
+
+    saludo();
+
+    count($arreglo);
+
+    print_r($variable);
+
+?>
