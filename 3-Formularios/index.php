b43bd220b650f8116df28d6bbc51832bf9a7feb2
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-06 08:10:28 +0200

    Termino la parte de enviando datos formulario

--- 3-Formularios/index.php
+++ 3-Formularios/index.php
@@ -9,7 +9,24 @@
 <body>
 
     <form action="recibe.php" method="">
-        <input type="text" placeholder="Nombre: " name="nombre">
+        <input type="text" placeholder="Nombre:" name="nombre"> <!-- El name me permite conectarme con php -->
+        <br>
+        <label for="hombre">Hombre</label>
+        <input type="radio" name="sexo" value="hombre" id="hombre">
+        
+        <label for="mujer">Mujer</label>
+        <input type="radio" name="sexo" value="mujer" id="mujer">
+
+        <select name="anyo" id="anyo">
+            <option value="1999">1999</option>
+            <option value="2000">2000</option>
+            <option value="2001">2001</option>
+        </select>
+
+        <label for="terminos">¿Aceptas los términos de usuario?</label>
+        <input type="checkbox" name="terminos" id="terminos" value="ok">
+        </br>
+
         <input type="submit" value="Enviar">
 
     </form>
