8b7846090d5d861dc913065cb9607b18470c16f7
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-10-13 12:18:02 +0200

    Empezada la practica del blog

---
+++ 15-Blog-Practica/admin/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('RUTA', 'http://localhost/15-Blog-Practica/');
+
+$bd_config = array (
+    'basedatos' => 'blog',
+    'usuario' => 'root',
+    'pass' => ''
+);
+
+$blog_config = array (
+    'post_pag' => '2',
+    'carpeta_imagenes' => 'imagenes/'
+);
---
+++ 15-Blog-Practica/views/index.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0,
+    maximum-scale=1.0, minimum-scale=1.0">
+    <title>Blog de Yaiza</title>
+    <script src="https://kit.fontawesome.com/fd11422aa1.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <div class="logo izquierda">
+                <p><a href="#">Este es mi blog :)</a></p>
+            </div>
+
+            <div class="derecha">
+                <form action="<?php  ?>"></form>
+            </div>
+
+        </div>
+    </header>
+</body>
+</html>
